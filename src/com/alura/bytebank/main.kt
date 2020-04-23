baadb78ba751ab8dfc94eacf1d9b0e95334dcf30
unknown <viiktoor@gmail.com>
2020-04-23 11:25:31 -0400

    nulls, chain call, scope functions, elvis operator, cast

--- src/com/alura/bytebank/main.kt
+++ src/com/alura/bytebank/main.kt
@@ -1,14 +1,18 @@
 package com.alura.bytebank
 
-import com.alura.bytebank.exception.InsufficientFoundsException
-import com.alura.bytebank.tests.testAccount
+import com.alura.bytebank.model.Address
 
 fun main() {
-    println("main has started")
-    testAccount()
-//    functionOne()
-    println("main has ended")
+//    val address: Address? = Address(street = "yonge st")
+//    address.let {
+//       println(address?.street?.length) //example of chain call
+//    }
 
+    val address: Address? = Address(street = "yonge st")
+    address?.let {
+        println(address.street.length) //example of safe call combined with LET scope function
+        println(address.street)
+    }
 }
 
 
